cfba9f90dbb3badfea3a5307fe6e958f85ce902e
Osid ALkourd <alkordosid73@gmail.com>
2025-08-04 14:09:51 +0300

    create ticket controller

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,15 @@
 import express from "express";
 import cors from 'cors';
 import dotenv from 'dotenv'
+import { serve } from "inngest/express";
+
 import connectDB from "./db/database.js";
 import userRoutes from "./routes/userRouter.js";
+import ticketRoutes from "./routes/ticketRouter.js";
+import { inngest } from "./services/inngest/client.js";
+import { onUserSignup } from "./services/inngest/functions/on-signup.js";
+import { onTicketCreated } from "./services/inngest/functions/on-ticket-create.js";
+
 
 dotenv.config();
 
@@ -13,6 +20,17 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth',userRoutes);
+app.use("/api/tickets", ticketRoutes);
+
+// the following route will recieve any event from inggest server (inggest server send request to my backend app)
+app.use(
+  "/api/inngest",
+  serve({
+    client: inngest,
+    functions: [onUserSignup, onTicketCreated],
+  })
+);
+
 
 connectDB();
 
